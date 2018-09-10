cdc5b3368175d9c2db64e441a7b8aff3ddc8e06a
Katrina Pritchard <pritckat@gmail.com>
2018-09-10 15:46:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class App < Sinatra::Base
     "My name is Katrina."
   end
 
+  get '/hometown' do
+    "My hometown is Benbrook, TX."
+  end
 
+  get '/favorite-song' do
+    "My favorite song is Fancy."
+  end
 end
