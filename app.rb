8718efaf4f18cf7040525e8b302f7ac3fe814aec
Katrina Pritchard <pritckat@gmail.com>
2018-09-10 15:46:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Katrina."
+  end
+
+  get '/hometown' do
+    "My hometown is Benbrook, TX."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fancy."
+  end
 end
